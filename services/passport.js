1f907a89d11cbb41b37012a7a976303b805e5706
Nan <nanliao917@gmail.com>
2022-07-14 20:13:11 -0700

    why

--- services/passport.js
+++ services/passport.js
@@ -19,10 +19,13 @@ passport.deserializeUser((id, done) => {
 
 
 passport.use(new GoogleStrategy({
+
 	clientID: keys.googleClientID,
 	clientSecret:  keys.googleClientSecret,
-	callbackURL: '/auth/google/callback'
-	}, (accessToken, refreshToken, profile, done) => {
+	callbackURL: '/auth/google/callback',
+	proxy:true
+	}, 
+	(accessToken, refreshToken, profile, done) => {
 		User.findOne({ googleId : profile.id })
 			.then((existingUser) => {
 				if (existingUser) {
