cc9fa261f87a5bfb691a7376dd02c4c8a158f3bd
Nan <nanliao917@gmail.com>
2022-08-12 14:12:24 -0700

    init

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div style = {{ textAlign:'center' }}>
+		<h1>
+			hi! 
+		</h1>
+		collect feedback
+		</div>
+		);
+};
+
+export default Landing; 
+
